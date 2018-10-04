d649e1bf8903ac4aa2c4e4da38c73bded0e1e71d
PuaChris <chris.pua@mail.utoronto.ca>
2018-10-04 15:22:08 -0400

    Playing around with Bottle variables for tutorial

--- main_page.py
+++ main_page.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
-print("Hello!")
-=======
-from bottle import route, run
-@route('/')
+from bottle import Bottle, run, template
+
+app = Bottle()
+
+@app.route('/')
 def hello():
     return "Hello World"
 
-run(host='localhost', port=8080, debug=True)
->>>>>>> 3558471af645669105117e07cfa4805ca6a10295
+run(app, host='localhost', port=8080)
