259fb25600813c88d17e93f089fbceb3dfb44411
Erick <erickmu1@hotmail.com>
2018-10-03 21:27:53 -0400

    Made 1) dictionary that maps: doc_id to doc_idx, 2) identified lexicon == _word_id_cache, 3) dictionary that maps: word_id to doc_id(s). Updated the following methods of crawler class: __init__(), _add_words_to_document(), _add_text().

--- main_page.py
+++ main_page.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
+
 print("Hello!")
-=======
+
 from bottle import route, run
 @route('/')
 def hello():
     return "Hello World"
 
 run(host='localhost', port=8080, debug=True)
->>>>>>> 3558471af645669105117e07cfa4805ca6a10295
