e77bc23bc46c51982b7297bc5f7ef60c7de83e34
PuaChris <chris.pua@mail.utoronto.ca>
2018-10-06 21:07:51 -0400

    Fixed parsing of string for spaces and display user keywords and top 20 popular searches and counts

--- main_page.py
+++ main_page.py
@@ -1,44 +1,47 @@
 from bottle import run, route, request, template, response
-from collections import OrderedDict, Counter
+from collections import Counter
+
+total_list = Counter()
 
 #with the line below, it's used for the server to find the site for hosting AND
 #it takes the route below it to be the first page the user sees
 @route('/') 
 def keywords():
-    return template('website_template')
+    return template('website_template', keywords_list=[], popular_list=[])
 
 #does the method in the function have to match the method of the form in HTML?
 @route('/', method='POST')
 def display_keywords():
 
+    global total_list
+    num_words = 20
+
     #get string from query submission
     #lower() makes it turn into lowercase
-    #.rstrip() removes the white space at the end
-    keywords_input = request.forms.get('keywords').lower().rstrip()
+    keywords_input_raw = request.forms.get('keywords').lower()
+    keywords_input = " ".join(keywords_input_raw.split())
 
     #separating string into words and putting it into an array
     keywords_original_list = keywords_input.split(" ")
 
+    #recording number of occurrences for each word 
     keywords_list = Counter (keywords_original_list)
+
+    #can add counter objects together 
+    total_list += keywords_list
+
+
+    popular_list = total_list.most_common(num_words)
+
+    # for keyword in keywords_list:
+
+        # for popular in popular_list:
+
     
+    return template('website_template', keywords_list=keywords_list, popular_list=popular_list)
 
-    # keywords_list=[]
-
-    # for word in keywords_original_list:
-        # keywords_count = keywords_input.count(word)
-
-        #creating a pair of str and int
-        # keyword_pair = (word, keywords_count)
-
-        #keywords_list.append(keyword_pair)
-        # keywords_list.append(word)
-
-    # retains order of words from input after duplicates are removed
-    # keywords_list = sorted(set(keywords_list),key=keywords_list.index)
-    
-
-    return template('table_template', keywords_list = keywords_list)
-
+#WHY IS THE TOTAL NUMBER OF POPULAR WORD OCCURRENCES DOUBLING EACH TIME???
+#Figure out how to not include empty answers 
 
 run(host='localhost', port=8080, debug = True)
 
--- website_template.tpl
+++ website_template.tpl
@@ -7,8 +7,41 @@
 
         <form action="/" method="post">
             <input name="keywords" type="text" />
-            <input value="Count number of keywords" type="submit" />
+            <input value="Count" type="submit" />
         </form> 
+
+        <table id=”results”>
+
+            <tr>
+                <td><b>Word</b></td>
+                <td><b>Count</b></td>    
+            </tr>
+
+            % for keyword in keywords_list:
+                <tr>
+                    <td>{{keyword}}</td>
+                    <td>{{keywords_list[keyword]}}</td>
+                <tr>
+            %end
+
+        </table>
+
+        <table id="history">
+
+            <tr>
+                <td><b>Top 20 Words</b></td>
+                <td><b>Count</b></td>    
+            </tr>
+
+            % for popular in popular_list:
+                <tr>
+                    <td>{{popular[0]}}</td>
+                    <td>{{popular[1]}}</td>
+                <tr>
+            %end
+
+
+        </table>
 
     </body>
 </html> 
