46ccdfb50a1ee321aafe86e7dbb8fc219eeee29b
Erick <erickmu1@hotmail.com>
2018-12-02 14:45:05 -0500

    Fixed spell checker so that it works.

--- search.py
+++ search.py
@@ -1,32 +1,47 @@
 def spell_check(keyword, db_conn):
-
-    # Retrieve all words in database
-    word_list = []
-    for row in db_conn.cursor().execute("SELECT word FROM lexicon"):
-        word_list.append(row[0])
+    """ Iterates through every word in lexicon database and calculates the Levenshtein distance(keyword, word)
+        using Dynamic Programming formulation of the problem """
 
     # Calculate Levenshtein Distance
     closest_word = ''
     closest_dist = 100  # Make this infinity
 
-    for word in word_list:
-        # Make a (keyword x word) array
-        row = [0] * len(word)     # Each row has "word" columns
-        min_dist = row * len(keyword)   # There are "keyword" rows
+    # Retrieve words from database
+    for element in db_conn.cursor().execute("SELECT word FROM lexicon"):
 
-        for row in range(len(keyword)):
-            for col in range(len(word)):
+        word = element[0]
+
+        # Make a (keyword x word) array
+        min_dist = [[0 for c in range(len(word) + 1)] for r in range(len(keyword) + 1)]
+
+        # Calculate Levenshtein distance
+        for row in range(len(keyword) + 1):
+            for col in range(len(word) + 1):
+
                 # Base Case
                 if min(row, col) is 0:
                     min_dist[row][col] = max(row, col)
+
                 # Sub-problem Optimization
                 else:
                     min_dist[row][col] = min(min_dist[row-1][col] + 1, min_dist[row][col-1] + 1,
-                                             min_dist[row-1][col-1] + (1 if keyword[row] == word[col] else 0))
+                                             min_dist[row-1][col-1] + (1 if keyword[row - 1] != word[col - 1] else 0))
 
         # Update closest word
-        if min_dist[len(word) - 1][len(keyword) - 1] < closest_dist:
-            closest_dist = min_dist[len(word) - 1][len(keyword) - 1]
+        if min_dist[len(keyword)][len(word)] < closest_dist:
+            closest_dist = min_dist[len(keyword)][len(word)]
             closest_word = word
 
     return closest_word
+
+
+if __name__ == "__main__":
+
+    import sqlite3
+
+    # Set up db Connection
+    db_conn = sqlite3.connect("dbFile.db")
+    word = 'woogle'
+    print("Given word: ", word)
+    match = spell_check(word, db_conn)
+    print("Closest match: ", match)
